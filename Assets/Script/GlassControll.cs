e63d6b44e9814e4fc7c00e8750ebf7319eeebbd4
MoonKyoungHun <lsf8355@gmail.com>
2024-10-12 05:08:12 +0900

    [Feat] 안경 컨트롤러 생성
    안경 컨트롤러 생성

---
+++ Assets/Script/GlassControll.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class GlassControll : MonoBehaviour
+{
+    [SerializeField] ARFaceManager faceManager;
+
+    [SerializeField] GameObject glassPrefab;
+
+    private GameObject glassTracker;
+
+    private void Awake()
+    {
+        glassTracker = Instantiate(glassPrefab);
+
+    }
+
+    private void OnEnable()
+    {
+        faceManager.facesChanged += OnFaceChange;
+    }
+
+    private void OnDisable()
+    {
+        faceManager.facesChanged -= OnFaceChange;
+
+    }
+
+    private void OnFaceChange(ARFacesChangedEventArgs args)
+    {
+        // 추적중인 얼굴에 변경사항(위치, 회전)이 있을 때
+        if (args.updated.Count > 0)                 // 하나만 적용하는 중
+        {
+            // ARFace를 가져와서
+            ARFace face = args.updated[0];
+
+            Vector3 glassPos = face.transform.TransformPoint(face.vertices[6]);             // 6번 위치에 표시한다.
+            glassTracker.transform.position = glassPos;
+        }
+    }
+}
