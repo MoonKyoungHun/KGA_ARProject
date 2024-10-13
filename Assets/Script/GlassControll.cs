31baf8ad381ea94c39b4e67d0bfd54a2658f5f21
MoonKyoungHun <lsf8355@gmail.com>
2024-10-14 07:54:19 +0900

    [Feat] 안경 버튼 연결
    안경 관련 코드 작성
    버튼에 안경 코드 연결 진행
    Glasses 코드 삭제

--- Assets/Script/GlassControll.cs
+++ Assets/Script/GlassControll.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 
@@ -11,10 +9,15 @@ public class GlassControll : MonoBehaviour
 
     private GameObject glassTracker;
 
-    private void Awake()
+    public void Awake()
     {
-        glassTracker = Instantiate(glassPrefabs[0]);
+        for (int i = 0; i < glassPrefabs.Length; i++)
+        {
+            glassTracker = Instantiate(glassPrefabs[i]);
 
+            glassTracker.SetActive(false);
+        }
+       
     }
 
     private void OnEnable()
@@ -22,21 +25,51 @@ public class GlassControll : MonoBehaviour
         faceManager.facesChanged += OnFaceChange;
     }
 
-    private void OnDisable()
-    {
-        faceManager.facesChanged -= OnFaceChange;
-    }
+    //private void OnDisable()
+    //{
+    //    faceManager.facesChanged -= OnFaceChange;
+    //}
 
     private void OnFaceChange(ARFacesChangedEventArgs args)
     {
         // 추적중인 얼굴에 변경사항(위치, 회전)이 있을 때
-        if (args.updated.Count > 0)                 // 하나만 적용하는 중
+        if (args.updated.Count > 0)                 
         {
             // ARFace를 가져와서
             ARFace face = args.updated[0];
 
             Vector3 glassPos = face.transform.TransformPoint(face.vertices[6]);             // 6번 위치에 표시한다.
             glassTracker.transform.position = glassPos;
+            glassTracker.SetActive(true);
         }
+        else if(args.removed.Count > 0)
+        {
+            glassTracker.SetActive(false);
+        }
+    }
+
+    public void Glass1()
+    {
+        glassTracker = Instantiate(glassPrefabs[0]);
+    }
+
+    public void Glass2()
+    {
+        glassTracker = Instantiate(glassPrefabs[1]);
+    }
+
+    public void Glass3()
+    {
+        glassTracker = Instantiate(glassPrefabs[2]);
+    }
+
+    public void Glass4()
+    {
+        glassTracker = Instantiate(glassPrefabs[3]);
+    }
+
+    public void Glass5()
+    {
+        glassTracker = Instantiate(glassPrefabs[4]);
     }
 }
